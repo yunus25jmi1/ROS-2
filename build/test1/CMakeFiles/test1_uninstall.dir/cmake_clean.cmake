95511428237e7a5146b65adce03784144f72d99a
Md Yunus <admin@yunuscollege.eu.org>
2024-09-15 00:37:58 +0530

    Workshop

---
+++ build/test1/CMakeFiles/test1_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test1_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/test1_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
